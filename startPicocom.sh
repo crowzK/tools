1524e73f98671a2be1512624cf12a9bb54501ffc
Yudoc Kim <yudoc@aiincorporated.com>
2019-07-31 10:14:42 -0400

    change to use variable

--- startPicocom.sh
+++ startPicocom.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+usb_dev="/dev/ttyUSB0"
+baudrate="921600"
 time=`date '+%Y_%m_%dT%H_%M_%S'`
 filename="./$time.log"
 
 echo "Create $filename"
-picocom -b 921600 --imap lfcrlf /dev/ttyUSB0 | tee -a $filename
+picocom -b $baudrate --imap lfcrlf $usb_dev | tee -a $filename
 
 #picocom -b 921600 --imap lfcrlf /dev/ttyUSB0 | awk '{ print strftime("%c%N: "), $0; fflush(); }' | tee serial.txt
 

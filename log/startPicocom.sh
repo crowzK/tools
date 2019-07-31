23fea14ca4e327661b7f9aab2b87d22311471a12
Yudoc Kim <yudoc@aiincorporated.com>
2019-07-31 10:45:53 -0400

    add time line

--- log/startPicocom.sh
+++ log/startPicocom.sh
@@ -6,12 +6,7 @@ time=`date '+%Y_%m_%dT%H_%M_%S'`
 filename="./$time.log"
 
 echo "Create $filename"
-picocom -b $baudrate --imap lfcrlf $usb_dev | tee -a $filename
+#picocom -b $baudrate --imap lfcrlf $usb_dev | tee -a $filename
 
-#picocom -b 921600 --imap lfcrlf /dev/ttyUSB0 | awk '{ print strftime("%c%N: "), $0; fflush(); }' | tee serial.txt
-
-#picocom -b 921600 --imap lfcrlf /dev/ttyUSB0 | awk '{ print strftime("%m/%d/%Y %H:%M:%S: ", systime()); fflush(); }' | tee serial.txt
-
-
-
+picocom -b $baudrate --imap lfcrlf $usb_dev | awk '{ "date +T%H:%M:%S:%3N:" |& getline timestamp; print timestamp,$0; close("date +T%H:%M:%S:%3N:"); fflush(); }' | tee -a $filename
 

659744ddfea5901e82c7571ca188118f3b7c18b8
Yudoc Kim <yudoc@aiincorporated.com>
2019-08-14 11:41:50 -0400

    change timestamp

--- log/startPicocom.sh
+++ log/startPicocom.sh
@@ -8,5 +8,12 @@ filename="./$time.log"
 echo "Create $filename"
 #picocom -b $baudrate --imap lfcrlf $usb_dev | tee -a $filename
 
-picocom -b $baudrate --imap lfcrlf $usb_dev | awk '{ "date +T%H:%M:%S:%3N:" |& getline timestamp; print timestamp,$0; close("date +T%H:%M:%S:%3N:"); fflush(); }' | tee -a $filename
+picocom -b $baudrate --imap lfcrlf $usb_dev | awk '{ "date +[T%H:%M:%S:%3N]" |& getline timestamp; print timestamp,$0; close("date +[T%H:%M:%S:%3N]"); fflush(); }' | tee -a $filename
+
+
+#picocom -b 921600 --imap lfcrlf /deb/ttyUSB0 | awk '{ "date +T%H:%M:%S:%3N:" |& getline timestamp; print timestamp,$0; close("date +T%H:%M:%S:%3N:"); fflush(); }' | tee -a test.log
+
+
+
+
 
